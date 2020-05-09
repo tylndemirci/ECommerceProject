afdd6cd4d6eb740f8071546badf926c07eaef054
Taylan Demirci <t4yl4n1@gmail.com>
2020-05-10 00:02:21 +0300

    Cart, CartLine entity; cartservice, manager added

---
+++ ECommerceProject.Entities/DomainModels/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProject.Entities.DomainModels
+{
+   public class Cart
+    {
+        public Cart()
+        {
+            CartLines = new List<CartLine>();
+        }
+
+        public List<CartLine> CartLines { get; set; }
+    }
+}
---
+++ ECommerceProject.Entities/DomainModels/CartLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ECommerceProject.Entities.Concrete;
+
+namespace ECommerceProject.Entities.DomainModels
+{
+  public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        
+    }
+}
