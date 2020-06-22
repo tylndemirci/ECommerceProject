db36b3cf36fe17a68c5ca9af627cdee62df4c264
Taylan Demirci <t4yl4n1@gmail.com>
2020-06-22 23:52:29 +0300

    IdentityRoles added, Index for roles and create action created.

---
+++ ECommerceProject.AdminUI/Controllers/AdminRoleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceProject.AdminUI.Controllers
+{
+    public class AdminRoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminRoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(string name)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var resultError in result.Errors)
+                    {
+                        ModelState.AddModelError("", resultError.Description);
+                    }
+                }
+            }
+
+            return View(name);
+        }
+    }
+}
