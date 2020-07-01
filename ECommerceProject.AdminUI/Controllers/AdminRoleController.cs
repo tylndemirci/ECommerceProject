ae4946553656a0fef20d8374b07d0f9651b987ca
Taylan Demirci <t4yl4n1@gmail.com>
2020-07-02 00:03:35 +0300

    fix for previous modal update in roles index.

--- ECommerceProject.AdminUI/Controllers/AdminRoleController.cs
+++ ECommerceProject.AdminUI/Controllers/AdminRoleController.cs
@@ -62,13 +62,12 @@ namespace ECommerceProject.AdminUI.Controllers
             return View(model);
         }
 
-        public IActionResult UsersInRole(string id, int pageIndex = 1)
+        public IActionResult UsersInRole(string id)
         {
-            var pageSize = 6;
-            int excludeRecords = (pageSize * pageIndex) - pageSize;
+           
             var role = _roleManager.FindByIdAsync(id);
             var users = _userManager.GetUsersInRoleAsync(role.Result.Name);
-            var returnModel = users.Result.ToList().Skip(excludeRecords).Take(pageSize).Select(x =>
+            var returnModel = users.Result.Select(x =>
                 new ViewUsersViewModel(x));
             return PartialView("_UsersInRolePartial", returnModel);
             
